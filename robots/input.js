c1ca63764c0d89bd295415ce46d011675a4022ab
lacort <lacort2@gmail.com>
2019-04-01 23:45:58 -0300

    feat: language pt

--- robots/input.js
+++ robots/input.js
@@ -8,12 +8,23 @@ function robot() {
 
   content.searchTerm = askAndReturnSearchTerm()
   content.prefix = askAndReturnPrefix()
+  content.lang = askAndReturnLanguage()
   state.save(content)
 
   function askAndReturnSearchTerm() {
     return readline.question('Type a Wikipedia search term: ')
   }
 
+  function askAndReturnLanguage(){
+    const language = ['pt','en']
+    const selectedLangIndex = readline.keyInSelect(language,'Choice Language: ')
+    const selectedLangText = language[selectedLangIndex]
+    return selectedLangText
+  }
+
+  
+
+  
   function askAndReturnPrefix() {
     const prefixes = ['Who is', 'What is', 'The history of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
