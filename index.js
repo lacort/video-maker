63f670cdebced5a60ec4cc6f66382002968b2fc9
lacort <lacort2@gmail.com>
2019-04-23 22:13:37 -0300

    resolved video and image conflit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const robots = {
     text: require('./robots/text.js'),
     state: require('./robots/state.js'),
     image: require('./robots/image.js'),
-    //video: require('./robots/video.js')
+    video: require('./robots/video.js')
 
     
 }
@@ -14,7 +14,7 @@ async function start(){
     robots.input()
     await robots.text()
     await robots.image()
-    //await robots,video()
+    await robots.video()
 
     const content = robots.state.load()
     console.dir(content, { depth: null})
